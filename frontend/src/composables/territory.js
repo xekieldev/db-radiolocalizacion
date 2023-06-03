50028e1a3980db6a9891c7c3a83190d748e799df
Exequiel Sartor <exequielsartor@gmail.com>
2023-06-03 15:56:13 -0300

    Add provinces and cities selectors, and others

--- frontend/src/composables/territory.js
+++ frontend/src/composables/territory.js
@@ -2,22 +2,35 @@ import { ref } from 'vue'
 import provincesDataRaw from "../../../data/provincias.json"
 import citiesDataRaw from "../../../data/localidades.json"
 
+function capitalizeWords(str) {
+  return str
+      .toLowerCase()
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ')
+}
+
+
+
 const provincesData = provincesDataRaw.provincias
 const citiesData = citiesDataRaw.localidades
-console.log(citiesData)
-console.log(provincesData)
+// console.log(citiesData.map(capitalizeWords(citiesData.nombre)))
+
+// const e = "HOLA"
+// console.log(capitalizeWords(e))
+// console.log(citiesData)
+// console.log(provincesData)
 
 export function useTerritory() {
   const provinces = ref([])
   const cities = ref([])
   
   function getTerritories() {
-    provinces.value = provincesData.map(p => ({ label: p.nombre, value: p.id }))
+    provinces.value = provincesData.map(p => ({ label: p.nombre.toUpperCase(), value: p.id }))
     cities.value = citiesData.filter(c => c.provincia.id === "54").map(c => ({ label: c.nombre, value: c.id }))
   }
   
   function getProvinceCities(provinceId) {
-    debbuger
     cities.value = citiesData.filter(c => c.provincia.id === provinceId).map(c => ({ label: c.nombre, value: c.id }))
   }
   
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -48,6 +48,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ListView.vue')
+    },
+    {
+      path: '/tech',
+      name: 'tech',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/FormTMView.vue')
     }
 
   ]
