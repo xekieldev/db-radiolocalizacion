4b1224479f980c2f731886f01429a88a92fc83ba
Exequiel Sartor <exequielsartor@gmail.com>
2023-08-08 15:51:12 -0300

    Added controllers to filex, station and tech_measurement and modified __init__.py and the models in db.py

---
+++ backend/src/filex.py
@@ -0,0 +1,73 @@
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from werkzeug.exceptions import abort
+from src.db import db, Filex, ma
+from sqlalchemy import exc
+
+
+
+
+bp = Blueprint("filex", __name__)
+
+
+class FilexSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ("id", "expediente", "fecha", "hora", "area", "id_technician1", "id_technician2" ) #, "technicians")
+
+filex_schema = FilexSchema()
+filexs_schema = FilexSchema( many = True )
+
+@bp.route('/file/<id>', methods = ['GET'])
+def get_file(id):
+    try:
+        # import pdb; pdb.set_trace()
+        filex = Filex.query.get(id)
+        return filex_schema.dump(filex)
+    except:
+        response = {"message": "input error"}
+        return response, 400
+    
+@bp.route("/file/", methods = ['GET'])
+def get_all_files():
+    try:
+        all_files = Filex.query.all()
+        # import pdb; pdb.set_trace()
+        return filexs_schema.dump(all_files)
+    except:
+        response = {"message": "server error"}
+        return response, 500
+
+
+@bp.route("/file", methods=["POST"])
+def filex():
+    try:
+        expediente = request.json.get('expediente')
+        fecha = request.json.get('fecha')
+        hora = request.json.get('hora')
+        area = request.json.get('area')
+        id_technician1 = request.json.get('id_technician1')
+        id_technician2 = request.json.get('id_technician2')
+        # technicians = request.json.get('technicians')
+        filex = Filex(expediente = expediente, fecha = fecha, hora = hora, area = area, id_technician1 = id_technician1, id_technician2 = id_technician2 ) #, technicians = technicians)
+        r = db.session.add(filex)
+        db.session.commit()
+        response = {"id": filex.id }
+        # import pdb; pdb.set_trace()
+        return response, 201 
+    except exc.SQLAlchemyError:
+        response = { "message": "database error" }
+        return response, 500
+    except:
+        response = { "message": "input error" }
+        return response, 400
+
+    
+
+
+
---
+++ backend/src/station.py
@@ -0,0 +1,76 @@
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from werkzeug.exceptions import abort
+from src.db import db, Station, ma
+
+
+
+bp = Blueprint("station", __name__)
+
+
+class StationSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ("identificacion", "emplazamiento", "servicio", "frecuencia", "unidad", "claseEmision", "irradiante", "polarizacion", "cantidad", "altura", "tipoVinculo", "frecuenciaVinc", "unidadVinc", "irradianteVinc", "polarizacionVinc", "id_location")
+
+station_schema = StationSchema()
+stations_schema = StationSchema(many=True)
+
+@bp.route('/station/<id>', methods = ['GET'])
+def get_station(id):
+    try:
+        # import pdb; pdb.set_trace()
+        station = Station.query.get(id)
+        return station_schema.dump(station)
+    except:
+        response = {"message": "input error"}
+        return response, 400
+    
+@bp.route("/station/", methods = ['GET'])
+def get_all_stations():
+    try:
+        all_stations = Station.query.all()
+        # import pdb; pdb.set_trace()
+        return stations_schema.dump(all_stations)
+    except:
+        response = {"message": "server error"}
+        return response, 500
+
+
+@bp.route("/station", methods = ['POST'])
+def station():
+    try:
+        import pdb; pdb.set_trace()
+
+        identificacion = request.json.get('identificacion')
+        emplazamiento = request.json.get('emplazamiento')
+        servicio = request.json.get('servicio')
+        frecuencia = request.json.get('frecuencia')
+        unidad = request.json.get('unidad')
+        claseEmision = request.json.get('claseEmision')
+        irradiante = request.json.get('irradiante')
+        polarizacion = request.json.get('polarizacion')
+        cantidad = request.json.get('cantidad')
+        altura = request.json.get('altura')
+        tipoVinculo = request.json.get('tipoVinculo')
+        frecuenciaVinc = request.json.get('frecuenciaVinc')
+        unidadVinc = request.json.get('unidadVinc')
+        irradianteVinc = request.json.get('irradianteVinc')
+        polarizacionVinc = request.json.get('polarizacionVinc')
+        id_location = request.json.get('id_location')
+        
+        station = Station(identificacion = identificacion, emplazamiento = emplazamiento, servicio = servicio, frecuencia = frecuencia, unidad = unidad, claseEmision = claseEmision, irradiante = irradiante, polarizacion = polarizacion, cantidad = cantidad, altura = altura, tipoVinculo = tipoVinculo, frecuenciaVinc = frecuenciaVinc, unidadVinc = unidadVinc, irradianteVinc = irradianteVinc, polarizacionVinc = polarizacionVinc, id_location = id_location )
+        
+        r = db.session.add(station)
+        db.session.commit()
+        response = {"id": station.id }
+
+        return response, 201 
+    except Exception as e:
+        response = {"message": "input error"}
+        return response, 400
