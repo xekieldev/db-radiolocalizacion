8a2835687d28015dedf65300ddc4da0a7bdfa3bd
Exequiel Sartor <exequielsartor@gmail.com>
2023-12-15 01:23:12 -0300

    Add List view from API

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,11 +1,14 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
+
 
 
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
     # app.config.from_mapping(
     #     # a default secret that should be overridden by instance config
     #     SECRET_KEY="dev",
