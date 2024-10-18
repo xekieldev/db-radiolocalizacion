925951662a79986e4745662c7918c1735e2257cb
Exequiel Sartor <exequielsartor@gmail.com>
2024-10-18 08:26:17 -0300

    Add marker cluster group to files map

--- frontend/src/components/GeneralMap.vue
+++ frontend/src/components/GeneralMap.vue
@@ -1,89 +1,102 @@
 <template>
   <div class="global-map-container">
-    <l-map
-      ref="map"
-      :zoom="zoom"
-      :center="[position[0] , position[1]]"
-      :use-global-leaflet="false"
-      :options="{ zoomControl: false }"
-      :min-zoom="1"
-      draggable="false"
-      dragging="false"
+    <l-map 
+    ref="map"
+    :zoom="zoom"
+    :useGlobalLeaflet="true" 
+    :center="[position[0], position[1]]"
+    :options="{ zoomControl: false }"
+    :min-zoom="1"
+    draggable="false"
+    dragging="false"
     >
-      <l-tile-layer
+    <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         layer-type="base"
         name="OpenStreetMap"
         :max-zoom="19"
       />
-      <l-marker 
-        v-for="item in files"
-        :key="item.id"
-        :lat-lng="[ item.latitud , item.longitud ]"
-      >
-        <l-icon
-          v-if="item.tipo"
-          :icon-size="[25, 25]"
-          :icon-url="pickIcon(item)"
-          :icon-anchor="[12, 12]"
-        />
-        <l-icon
-          v-else
-          :icon-size="[40, 25]"
-          :icon-url="pickIcon(item)"
-          :icon-anchor="[12, 12]"
-        />
-        <l-popup
-          :options="{}"
-          class="popup-content"
+      <l-marker-cluster-group>
+        <l-marker 
+          v-for="item in files"
+          :key="item.id"
+          :lat-lng="[ item.latitud , item.longitud ]"
         >
-          <p class="popup-title">
-            {{ item.expediente }}
-          </p>
-          <prop-value
-            v-if="item.aeropuerto"
-            class="prop"
-            label="Aeropuerto"
-            :value="item.aeropuerto"
+          <l-icon
+            v-if="item.tipo"
+            :icon-size="[25, 25]"
+            :icon-url="pickIcon(item)"
+            :icon-anchor="[12, 12]"
           />
-          <prop-value
-            v-if="item.localidad"
-            class="prop"
-            label="Localidad (Provincia)"
-            :value="getNameByCode('city', item.localidad) + ' (' + getNameByCode('province', item.provincia)+')'"
+          <l-icon
+            v-else
+            :icon-size="[40, 25]"
+            :icon-url="pickIcon(item)"
+            :icon-anchor="[12, 12]"
           />
-          <prop-value
-            v-if="item.frecuencia"
-            class="prop"
-            label="Frecuencia"
-            :value="item.frecuencia + ' ' + item.unidad"
-          />
-          <my-button
+          <l-popup
+            :options="{}"
+            class="popup-content"
+          >
+            <p class="popup-title">
+              {{ item.expediente }}
+            </p>
+            <prop-value
+              v-if="item.aeropuerto"
+              class="prop"
+              label="Aeropuerto"
+              :value="item.aeropuerto"
+            />
+            <prop-value
+              v-if="item.localidad"
+              class="prop"
+              label="Localidad (Provincia)"
+              :value="getNameByCode('city', item.localidad) + ' (' + getNameByCode('province', item.provincia)+')'"
+            />
+            <prop-value
+              v-if="item.frecuencia"
+              class="prop"
+              label="Frecuencia"
+              :value="item.frecuencia + ' ' + item.unidad"
+            />
+            <my-button
               class="primary view-button"
               label="Ver"
               @on-tap="() => viewItem(item.id)"
-          />
-        </l-popup>
-      </l-marker>
+            />
+          </l-popup>
+        </l-marker>
+      </l-marker-cluster-group>
     </l-map>
   </div>
 </template>
 
 <script setup>
-import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer, LMarker, LIcon, LPopup } from "@vue-leaflet/vue-leaflet";
+import L from 'leaflet'
+// globalThis.L = L
+import { LMap, LTileLayer, LIcon, LPopup, LMarker } from '@vue-leaflet/vue-leaflet'
+import { LMarkerClusterGroup } from 'vue-leaflet-markercluster'
+import 'leaflet/dist/leaflet.css'
+import 'vue-leaflet-markercluster/dist/style.css'
+import { useIconsMap } from "../composables/iconsmap"
+import genericIcon from "../../img/map_icons/generic.png"
 import { useTerritory } from "../composables/territory";
 import MyButton from "./MyButton.vue";
 import { useRouter } from 'vue-router'
 import PropValue from "./PropValue.vue";
-import { useIconsMap } from "../composables/iconsmap"
-import genericIcon from "../../img/map_icons/generic.png"
 
-
-const { getNameByCode } = useTerritory()
 const { getIconUrl } = useIconsMap()
+const { getNameByCode } = useTerritory()
 const router = useRouter()
 
+const props = defineProps({
+  position: {
+    type: Array,
+    default: () => [0, 0]
+  },
+  zoom: Number,
+  files: Array,
+})
 
 function pickIcon(item) {
   let icon = ''
@@ -97,15 +110,6 @@ function pickIcon(item) {
   return icon
 }
 
-const props = defineProps({
-  position: {
-    type: Array,
-    default: () => [0, 0]
-  },
-  zoom: Number,
-  files: Array,
-})
-
 function viewItem(file_id) {  
   router.push(`/file/${file_id}`)
 }
@@ -113,7 +117,6 @@ function viewItem(file_id) {
 </script>
 
 <style scoped>
-
 .global-map-container {
   height: 700px;
   width: 100%;
