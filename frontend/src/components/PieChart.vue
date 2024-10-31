3c4219a175c396a73030ed809c52ed0c7963e3f4
Exequiel Sartor <exequielsartor@gmail.com>
2024-10-31 09:27:28 -0300

    Add pie chart to statistics view among other things

---
+++ frontend/src/components/PieChart.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Pie } from 'vue-chartjs'
+
+
+const props = defineProps({
+title: String,
+chartData: Object,
+})
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+
+</script>
+<template>
+    <Pie :data="chartData" :options="{
+      plugins: {
+        legend: {
+          display: true,
+          position: 'bottom',
+          labels: {
+            color: '#333', // Color de los labels
+            font: {
+              size: 10 // Tamaño de la fuente de los labels
+            }
+          }
+        }
+      },
+      responsive: true,
+      maintainAspectRatio: false
+    }"/>
+</template>
+
+<style scoped>
+
+</style>
