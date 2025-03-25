12f4d1524d4084d0944c9f4203eab151d4e4bb1a
Exequiel Sartor <exequielsartor@gmail.com>
2025-03-25 10:07:48 -0300

    Add new regex validation cases

--- frontend/src/composables/filevalidation.js
+++ frontend/src/composables/filevalidation.js
@@ -5,11 +5,14 @@ export function useFileValidation() {
     const fileRegEx3 = /^NO-\d{4}-\d{8}-[A-Z]{3,8}-[A-Z]{2,10}#?[A-Z]{6}?$/
     const fileRegEx4 = /^[A-Z]{5,20}\s?E?\s?\s\d{1,8}\/\d{4}$/
     const fileRegEx5 = /^A\s{1}definir$/
+    const fileRegEx6 = /^No\s{1}aplica$/
+    const fileRegEx7 = /^PV-\d{4}-\d{8}-[A-Z]{3}-[A-Z]{2,5}#[A-Z]{6}$/
+
 
     const getComposedRegex = (...regexes) => new RegExp(regexes.map(regex => regex.source).join("|"))
 
 
-    const fileRegEx = getComposedRegex(fileRegEx1, fileRegEx2, fileRegEx3, fileRegEx4, fileRegEx5)
+    const fileRegEx = getComposedRegex(fileRegEx1, fileRegEx2, fileRegEx3, fileRegEx4, fileRegEx5, fileRegEx6, fileRegEx7)
     const validateFile = file => !!file.value.match(fileRegEx)
 
     return { validateFile }
